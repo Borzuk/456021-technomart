5a80668e435fa1e2d975b70069977359f760435e
Ivan <borzunovivan@gmail.com>
2017-09-09 20:42:49 +0300

    Исправление svg файлов под Хром  JS для батоннов в слайдере

--- js/script.js
+++ js/script.js
@@ -71,3 +71,28 @@ arrRight.addEventListener("click", function (evt) {
    slides[currentSlide].classList.toggle("visually-hidden");
    slidesControl[currentSlide].classList.toggle("active");
 });
+
+
+// Радио баттоны для слайдера
+
+var slider = document.querySelector('.slider');
+var controlRadio = slider.querySelectorAll('.slider__radio');
+var sliderItems = slider.querySelectorAll('.slider__item');
+var index = 0;
+
+slider.addEventListener('click', controlSliderRadio);
+
+function controlSliderRadio (evt) {
+  evt.preventDefault();
+  if(!evt.target.classList.contains('active') && evt.target.classList.contains('slider__radio')) {
+    for (var i=0; i < controlRadio.length; i++) {
+      if(controlRadio[i].classList.contains('active')) {
+        index = i;
+      }
+      controlRadio[i].classList.remove('active');
+      sliderItems[i].classList.remove('visually-hidden');
+    }
+    evt.target.classList.add('active');
+    sliderItems[index].classList.add('visually-hidden');
+  }
+}
